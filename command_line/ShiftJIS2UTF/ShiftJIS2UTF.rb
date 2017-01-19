44ee79f52a252474e1de1169ac31d10335ba1e3e
thirtythirty <qawsedr443@gmail.com>
2017-01-19 18:40:51 +0900

    ついでだからUTFからShiftJIS変換ツール
    前作ったやつのUTF8エンコードからShiftJISにする版

--- command_line/ShiftJIS2UTF/ShiftJIS2UTF.rb
+++ command_line/ShiftJIS2UTF/ShiftJIS2UTF.rb
@@ -1,13 +1,12 @@
-
-open_dir_name = './input_utf/'
-output_dir_name = './output_shift_jis/'
+open_dir_name = './input_shift_jis/'
+output_dir_name = './output_utf/'
 file_names = Dir.entries(open_dir_name)
 file_names.select! {|item| /[^.]+\.[^.]+/ === item }
 
 file_names.each_with_index do |file_name, index|
 	puts "-----------#{file_name}-----------"
-	File.open(open_dir_name + file_name, 'r',  encoding: "UTF-8:Shift_JIS") do |input|
-		output = File.open(output_dir_name + file_name, 'w', encoding: "Shift_JIS")
+	File.open(open_dir_name + file_name, 'r',  encoding: "Shift_JIS:UTF-8") do |input|
+		output = File.open(output_dir_name + file_name, 'w', encoding: "UTF-8")
 		output.puts(input.read)
 		output.close()
 	end
