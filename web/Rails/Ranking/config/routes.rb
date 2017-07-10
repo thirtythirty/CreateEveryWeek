f7a76e2c3b235c09d1710d97720406b56718ca83
thirtythirty <qawsedr443@gmail.com>
2017-07-10 13:48:40 +0900

    WIP ランキングシステム

--- web/Rails/Ranking/config/routes.rb
+++ web/Rails/Ranking/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :scores
-  resources :contests do
+  resources :contests, shallow: true do
     member do
       get 'ranking', to: 'ranking#ranking'
     end
+    resources :scores
   end
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
