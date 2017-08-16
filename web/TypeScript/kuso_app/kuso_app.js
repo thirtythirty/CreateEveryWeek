ffa1bb3ded9e4ae4e71918ad381dc112e9d4f561
thirtythirty <qawsedr443@gmail.com>
2017-08-16 11:38:16 +0900

    typescriptでjqueryを使って見た
    試しただけ

---
+++ web/TypeScript/kuso_app/kuso_app.js
@@ -0,0 +1,5 @@
+/// <reference path="typings/globals/jquery/index.d.ts" />
+$(document).ready(function () {
+    var jq = $("#content");
+    jq.append('aaa' + '<br>');
+});
---
+++ web/TypeScript/kuso_app/kuso_app.ts
@@ -0,0 +1,6 @@
+/// <reference path="typings/globals/jquery/index.d.ts" />
+
+$(document).ready(() => {
+  var jq:JQuery = $("#content")
+  jq.append('aaa' + '<br>')
+})
