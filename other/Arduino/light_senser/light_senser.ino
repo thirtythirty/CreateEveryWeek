3af9432d5641faff382c4bde1601c874e56f2685
thirtythirty <qawsedr443@gmail.com>
2017-10-10 13:43:14 +0900

    10/4 作成 レーザー避けゲーム強化
    レーザーの値の平均値を取り、そこからずれたらスピーカーがなるように
    要するに当たり判定の強化。ただ手でシュッとレーザーを遮っても早すぎて捉えられない...
    そこらへんは無視

--- other/Arduino/light_senser/light_senser.ino
+++ other/Arduino/light_senser/light_senser.ino
@@ -1,6 +1,9 @@
 float light_pre = 0;
 #define BEAT 300   // 音の長さを指定
 #define PINNO 11   // 圧電スピーカを接続したピン番号
+#define BUF_NUM 100
+int sensor_values[BUF_NUM];
+int count_for_senser_values;
 
 void setup() {
   // put your setup code here, to run once:
@@ -10,6 +13,12 @@ void setup() {
 
   pinMode(13,OUTPUT) ;  //ＬＥＤに接続ピンをデジタル出力に設定
   Serial.begin(9600);
+
+  count_for_senser_values = 0;
+  int i;
+  for(i = 0; i < BUF_NUM; i++){
+    sensor_values[i] = 0;
+  }
 }
 
 void loop() {
@@ -19,19 +28,31 @@ void loop() {
   float light_senser;
   light_senser=analogRead(0);
 
+  int sum = 0;
+  int i;
+  for(i = 0; i < BUF_NUM; i++){
+    sum += sensor_values[i];
+  }
+  float average = sum/(float)BUF_NUM;
   
-  
-  if(light_senser > 100){
+  if((float)light_senser > (average+3)){
     float light_diff = light_pre - light_senser;
-    if(light_diff < -50){
+//    if(light_diff < -50){
       tone(PINNO,262,BEAT) ;  // ド
-    }
+//    }
     digitalWrite(13,HIGH);
   } else {
     digitalWrite(13,LOW);
   }
   light_pre = light_senser;
+  // buf value
+  sensor_values[count_for_senser_values%BUF_NUM] = light_senser;
+  if(count_for_senser_values >= 10000){
+    count_for_senser_values = 0;
+  }
+  count_for_senser_values+=1;
   Serial.print(light_senser);
-//  Serial.print("\t");
+  Serial.print("\t");
+  Serial.print(average);
   Serial.println();
 }
